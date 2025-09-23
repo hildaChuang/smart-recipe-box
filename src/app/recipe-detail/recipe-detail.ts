1107b9699f3a0593f5e1fc8f955d3ff92ce2a0f3
hilda.chuang <hilda.chuang@gmail.com>
2025-09-23 06:00:42 +0000

    refactor: completed Module 9, signal input

---
+++ src/app/recipe-detail/recipe-detail.ts
@@ -0,0 +1,33 @@
+import { Component, computed, input, signal } from '@angular/core';
+import { RecipeModel } from '../models';
+
+@Component({
+  selector: 'app-recipe-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-detail.html',
+  styleUrl: './recipe-detail.css'
+})
+export class RecipeDetail {
+  readonly recipe = input.required<RecipeModel>();
+
+  protected servings = signal(1);
+  protected readonly adjustedIngredients = computed(() => {
+    const recipe = this.recipe();
+    const servings = this.servings();
+
+    return recipe.ingredients.map(ingredient =>({
+      ...ingredient,
+      quantity: ingredient.quantity * servings
+    }))
+  });
+
+  updateServings(isAdd: boolean) {
+    if(isAdd) {
+      this.servings.update(s => s + 1)
+    } else {
+      this.servings.update(s => s - 1)
+    }
+    console.log(this.servings())
+  }
+}
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,50 +1,27 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeModel } from '../models';
+import { RecipeDetail } from '../recipe-detail/recipe-detail';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RecipeDetail
+  ],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css'
 })
 export class RecipeList {
   protected readonly title = signal('My Recipe Box');
   recipes: RecipeModel[] = MOCK_RECIPES;
-  servings = signal(1);
   
-  protected currentRecipe = signal(this.recipes[0]);
-  protected adjustedIngredients = computed(() => {
-    const recipe = this.currentRecipe();
-    const servings = this.servings();
-    
-    return recipe.ingredients.map(ingredient =>({
-      ...ingredient,
-      quantity: ingredient.quantity * servings
-    }))
-  });
+  protected currentRecipe = signal<RecipeModel>(this.recipes[0]);
+
 
   // 一個受保護的方法，用來處理點擊事件
-  protected increment(no: number): void {
-    this.currentRecipe.set(this.recipes[no-1]);
-    switch(no) {
-      case 1:
-        console.log('Show first recipe');
-        break;
-      case 2:
-        console.log('Show second recipe');
-        break;
-    }
-    // 我們之後會學習如何更新 signal 的值
+  protected onSelect(no: number): void {
+    this.currentRecipe.set(this.recipes[no]);
   }
 
-  updateServings(isAdd: boolean) {
-    if(isAdd) {
-      this.servings.update(s => s + 1)
-    } else {
-      this.servings.update(s => s - 1)
-    }
-    console.log(this.servings())
-  }
 }
